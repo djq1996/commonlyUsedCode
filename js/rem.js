6e69fb96a0e06b05214d80f796d4c4e6f250dba4
董先生 <18513552894@163.com>
2018-01-20 22:58:28 +0800

    Update rem.js

--- js/rem.js
+++ js/rem.js
@@ -1,32 +1,3 @@
-<script type="text/javascript">
-      window.onload = function(){
-		    /*720代表设计师给的设计稿的宽度，你的设计稿是多少，就写多少;100代表换算比例，这里写100是
-		      为了以后好算,比如，你测量的一个宽度是100px,就可以写为1rem,以及1px=0.01rem等等*/
-		    getRem(720,100)
-		};
-		window.onresize = function(){
-		    getRem(720,100)
-		};
-		function getRem(pwidth,prem){
-		    var html = document.getElementsByTagName("html")[0];
-		    var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
-		    html.style.fontSize = oWidth/pwidth*prem + "px";
-		}
-		/*
-		//借鉴小米官网的换算
-		!function(n){
-		    var  e=n.document,
-		         t=e.documentElement,
-		         i=720,
-		         d=i/100,
-		         o="orientationchange"in n?"orientationchange":"resize",
-		         a=function(){
-		             var n=t.clientWidth||320;n>720&&(n=720);
-		             t.style.fontSize=n/d+"px"
-		         };
-		         e.addEventListener&&(n.addEventListener(o,a,!1),e.addEventListener("DOMContentLoaded",a,!1))
-		}(window);*/
-  </script>
 var docEl = document.documentElement;
 var fontEl = document.createElement('style');
 var metaEl = document.querySelector('meta[name="viewport"]');
@@ -35,7 +6,6 @@ dpr = window.devicePixelRatio || 1;
 rem = docEl.clientWidth * dpr / 10;
 scale = 1 / dpr;
 
-
 // 设置viewport，进行缩放，达到高清效果
 metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
 
@@ -43,19 +13,50 @@ metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-sc
 docEl.setAttribute('data-dpr', dpr);
 
 // 动态写入样式
+console.log(docEl.firstElementChild)
 docEl.firstElementChild.appendChild(fontEl);
 fontEl.innerHTML = 'html{font-size:' + rem + 'px!important;}';
 
 // 给js调用的，某一dpr下rem和px之间的转换函数
 window.rem2px = function(v) {
-    v = parseFloat(v);
-    return v * rem;
+	v = parseFloat(v);
+	return v * rem;
 };
 
 window.px2rem = function(v) {
-    v = parseFloat(v);
-    return v / rem;
+	v = parseFloat(v);
+	return v / rem;
 };
 
 window.dpr = dpr;
 window.rem = rem;
+
+window.onload = function() {
+	/*720代表设计师给的设计稿的宽度，你的设计稿是多少，就写多少;100代表换算比例，这里写100是
+	  为了以后好算,比如，你测量的一个宽度是100px,就可以写为1rem,以及1px=0.01rem等等*/
+	getRem(720, 100)
+};
+window.onresize = function() {
+	getRem(720, 100)
+};
+
+function getRem(pwidth, prem) {
+	var html = document.getElementsByTagName("html")[0];
+	var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+	html.style.fontSize = oWidth / pwidth * prem + "px";
+}
+
+//借鉴小米官网的换算
+! function(n) {
+	var e = n.document,
+		t = e.documentElement,
+		i = 720,
+		d = i / 100,
+		o = "orientationchange" in n ? "orientationchange" : "resize",
+		a = function() {
+			var n = t.clientWidth || 320;
+			n > 720 && (n = 720);
+			t.style.fontSize = n / d + "px"
+		};
+	e.addEventListener && (n.addEventListener(o, a, !1), e.addEventListener("DOMContentLoaded", a, !1))
+}(window);
