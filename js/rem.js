42a2d506c5a2b5dbe7cad325ecc67a8c0a2a692a
董先生 <18513552894@163.com>
2018-01-21 22:55:27 +0800

    Update rem.js

--- js/rem.js
+++ js/rem.js
@@ -46,6 +46,9 @@ function getRem(pwidth, prem) {
 	html.style.fontSize = oWidth / pwidth * prem + "px";
 }
 
+
+
+
 //借鉴小米官网的换算
 ! function(n) {
 	var e = n.document,
@@ -60,3 +63,26 @@ function getRem(pwidth, prem) {
 		};
 	e.addEventListener && (n.addEventListener(o, a, !1), e.addEventListener("DOMContentLoaded", a, !1))
 }(window);
+
+
+
+
+
+
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			if(!clientWidth) return;
+			if(clientWidth >= 750) {
+				docEl.style.fontSize = '100px';
+			} else {
+				docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+			}
+		};
+
+	if(!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
