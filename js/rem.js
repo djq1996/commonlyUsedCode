5dfefe2b4ec8b6e0d86fce09c5437e4ee9636017
Starry <1259505412@qq.com>
2018-07-13 17:41:06 +0800

    Update rem.js

--- js/rem.js
+++ js/rem.js
@@ -49,26 +49,10 @@ function getRem(pwidth, prem) {
 
 
 
-//借鉴小米官网的换算
-! function(n) {
-	var e = n.document,
-		t = e.documentElement,
-		i = 720,
-		d = i / 100,
-		o = "orientationchange" in n ? "orientationchange" : "resize",
-		a = function() {
-			var n = t.clientWidth || 320;
-			n > 720 && (n = 720);
-			t.style.fontSize = n / d + "px"
-		};
-	e.addEventListener && (n.addEventListener(o, a, !1), e.addEventListener("DOMContentLoaded", a, !1))
-}(window);
 
 
 
-
-
-
+// 750设计图   1rem == 50px
 (function(doc, win) {
 	var docEl = doc.documentElement,
 		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
@@ -86,3 +70,5 @@ function getRem(pwidth, prem) {
 	win.addEventListener(resizeEvt, recalc, false);
 	doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+
